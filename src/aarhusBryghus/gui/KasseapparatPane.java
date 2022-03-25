0d4314a49e0af6943c51c57d3dbe2bd74cf09d6a
Esben Sommer Bach <eaaesbac@students.eaaa.dk>
2022-03-25 09:56:29 +0100

    KasseapparatPane: Lavet knap til betaling

--- src/aarhusBryghus/gui/KasseapparatPane.java
+++ src/aarhusBryghus/gui/KasseapparatPane.java
@@ -21,6 +21,8 @@ import java.awt.*;
 public class KasseapparatPane extends GridPane {
     //TODO Lav tekstfields og andet stuff her:
     Label lblBetalingsmetoder;
+    Button btnBetaling;
+    RadioButton r1, r2, r3, r4;
 
     public KasseapparatPane() {
         this.setPadding(new Insets(20));
@@ -28,30 +30,30 @@ public class KasseapparatPane extends GridPane {
         this.setVgap(10);
         this.setGridLinesVisible(false);
 
-        //TODO Tilføj stuff til GUI:
-
-        Label lblTest = new Label("Test");
-
         //TODO Gør så knap virker og sat det rigtige sted
         //Knap til opret produkt
         Button btnOpretProdukt = new Button("Opret Produkt");
         this.add(btnOpretProdukt,0,0);
         GridPane.setHalignment(btnOpretProdukt, HPos.RIGHT);
         btnOpretProdukt.setOnAction(event -> this.opretProduktAction());
+        //TODO Lav knap færdig
+        btnBetaling = new Button("Betal");
+        this.add(btnBetaling,11,12);
+        GridPane.setHalignment(btnBetaling, HPos.RIGHT);
 
         lblBetalingsmetoder = new Label("Betalingsmetoder:");
         this.add(lblBetalingsmetoder,10,9);
         //TODO Går så man kun kan vælge en RadioButton af gangen
-        RadioButton r1 = new RadioButton("Kontant");
+        r1 = new RadioButton("Kontant");
         this.add(r1,10,10);
 
-        RadioButton r2 = new RadioButton("Mobilepay");
+        r2 = new RadioButton("Mobilepay");
         this.add(r2,10,11);
 
-        RadioButton r3 = new RadioButton("Dankort");
+        r3 = new RadioButton("Dankort");
         this.add(r3,10,12);
 
-        RadioButton r4 = new RadioButton("Klippekort");
+        r4 = new RadioButton("Klippekort");
         this.add(r4,10,13);
     }
 
