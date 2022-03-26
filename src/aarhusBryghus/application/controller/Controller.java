299de90b011fa14071d2d3bd28326a48b1d414e1
MikkelHauge <mikkelhauge84+github@gmail.com>
2022-03-27 00:47:14 +0100

    Tilføjet "singleton pattern" til storage og controller.
    begge kaldes med ".getInstance"

--- src/aarhusBryghus/application/controller/Controller.java
+++ src/aarhusBryghus/application/controller/Controller.java
@@ -1,6 +1,22 @@
 package aarhusBryghus.application.controller;
 
+import aarhusBryghus.storage.Storage;
+
 public class Controller {
+
+    private static Controller uniqueInstance;
+    private Storage storage;
+
+    public static Controller getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Controller();
+        }
+        return uniqueInstance;
+    }
+
+    private Controller() {
+        storage = storage.getInstance();
+    }
 
     public static void initStorage() {
 
--- src/aarhusBryghus/storage/Storage.java
+++ src/aarhusBryghus/storage/Storage.java
@@ -1,4 +1,38 @@
 package aarhusBryghus.storage;
 
+import aarhusBryghus.application.model.Produkt;
+
+import java.util.ArrayList;
+
 public class Storage {
+
+    private static Storage uniqueInstance;
+
+    public static Storage getInstance() {
+        if (uniqueInstance == null) {
+            uniqueInstance = new Storage();
+        }
+        return uniqueInstance;
+    }
+
+    private ArrayList<Produkt> produkter = new ArrayList<>();
+
+    // -------------------------------------------------------------------------
+
+    public ArrayList<Produkt> getCompanies() {
+        return new ArrayList<Produkt>(produkter);
+    }
+
+    public void addCompany(Produkt produkt) {
+        produkter.add(produkt);
+    }
+
+    public void removeCompany(Produkt produkt) {
+        produkter.remove(produkt);
+    }
+
+    // -------------------------------------------------------------------------
+
+
+    // -------------------------------------------------------------------------
 }
